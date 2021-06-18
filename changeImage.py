9a8c4d7ce4867cb478a9338e55311b3b88412b75
David Taxer <davidjtaxer@gmail.com>
2021-06-18 17:28:43 -0400

    changes line 18 so it splits file at the dot, keeps file name and add.jpeg when saving the file .split('.')[0] + ".jpeg"

--- changeImage.py
+++ changeImage.py
@@ -15,4 +15,4 @@ for files in os.listdir(path):
         if not files.startswith('.') and not files.startswith('README')and not files.startswith('LICENSE'):
             with Image.open(path + files) as im:
                 im2=im.convert('RGB').resize((600, 400))
-                im3=im2.save(path + files, 'jpeg', quality=100)
+                im3=im2.save(path + files.split('.')[0] + ".jpeg", 'jpeg')
