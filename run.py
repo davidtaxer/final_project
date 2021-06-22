abe41b5764bdaa53aec6aa7da2705ac0e8841cfb
David Taxer <davidjtaxer@gmail.com>
2021-06-22 16:59:28 -0400

    script prints a list of dictinaries containing the text files as the dictionaries with key value pairs for name, weight(converted to integer), and description

--- run.py
+++ run.py
@@ -12,22 +12,24 @@ description_path = cwd + "/supplier-data/descriptions/"
 image_path = cwd + "/supplier-data/images/"
 
 # create a list of descriptions
-items = os.listdir(description_path)
-
+text_files = os.listdir(description_path)
+image_files = os.listdir(image_path)
 # uncomment print line to check if items variable
 # creats a list of files in path correctly
 
-#print(items)
-
+# print(items)
+# print(images)
 #itterate through files in the directory and store as a dictionary
+list = []
+for file in text_files:
 
-for file in items:
-        data = []
-        with open (description_path + file) as info:
-                data = info.read().split('\n')
-                file_dict = {'name':data[0], 'weight':data[1], 'description':data[2], "image_name":data[3]}
-#                response = requests.post("http://34.69.160.243/fruits/", json=file_dict)
-#                print(response.status_code)
+        with open (description_path + file, 'r') as info:
+            data = (info.read().split('\n'))
+            file_dict = {'name':data[0], 'weight':int(data[1].split(" ")[0]), 'description':data[2]}
+            list.append(file_dict)
+#            response = requests.post("http://34.69.160.243/fruits/", json=file_dict)
+#            print(response.status_code)
 
+#for image in image_files:
 
-print(data)
+print(list)
