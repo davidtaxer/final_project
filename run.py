59756e74005fcaf1ce0152a80b3834bfce9e1750
David Taxer <davidjtaxer@gmail.com>
2021-06-22 19:36:06 -0400

    partially working script.  blakberry posts twice with image and the rest post without the image.  need debugging

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ image_path = cwd + "/supplier-data/images/"
 # create a list of descriptions
 text_files = os.listdir(description_path)
 image_files = os.listdir(image_path)
-# uncomment print line to check if items variable
+list_images = [image_name for image_name in image_files if '.jpeg' in image_name]
 # creats a list of files in path correctly
 
 # print(items)
@@ -26,13 +26,11 @@ for file in text_files:
             data = (info.read().split('\n'))
             file_dict = {'name':data[0], 'weight':int(data[1].split(" ")[0]), 'description':data[2]}
             list.append(file_dict)
-#            response = requests.post("http://34.69.160.243/fruits/", json=file_dict)
-#            print(response.status_code)
 
-for image in image_files:
+for image in list_images:
     if image.split('.')[0] in file.split('.')[0]:
         file_dict['image_name']=image
-    list.append(file_dict)
+        list.append(file_dict)
 
 for item in list:
     resp = requests.post('http://34.134.195.119/fruits/', json=item)
@@ -41,3 +39,4 @@ for item in list:
     print('Created feedback ID: {}'.format(resp.json()["id"]))
 
 #print(*list, sep = "\n")
+print(list_images)
