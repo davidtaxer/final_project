3387d5cb7ec14e245ac07d8b5eb95c1f2a71f8e0
David Taxer <davidjtaxer@gmail.com>
2021-06-22 18:21:08 -0400

    script added descriptions to the webpage but images did not load and  blaskberry description was added multiple times.  lines 32 through 41 are new

--- run.py
+++ run.py
@@ -22,7 +22,6 @@ image_files = os.listdir(image_path)
 #itterate through files in the directory and store as a dictionary
 list = []
 for file in text_files:
-
         with open (description_path + file, 'r') as info:
             data = (info.read().split('\n'))
             file_dict = {'name':data[0], 'weight':int(data[1].split(" ")[0]), 'description':data[2]}
@@ -30,6 +29,15 @@ for file in text_files:
 #            response = requests.post("http://34.69.160.243/fruits/", json=file_dict)
 #            print(response.status_code)
 
-#for image in image_files:
+for image in image_files:
+    if image.split('.')[0] in file.split('.')[0]:
+        file_dict['image_name']=image
+    list.append(file_dict)
 
-print(*list, sep = "\n")
+for item in list:
+    resp = requests.post('http://34.134.195.119/fruits/', json=item)
+    if resp.status_code != 201:
+        raise Exception('POST error status={}'.format(resp.status_code))
+    print('Created feedback ID: {}'.format(resp.json()["id"]))
+
+#print(*list, sep = "\n")
