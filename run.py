308d3f81b1806d3d51cc2f6c89e5743c28955831
David Taxer <davidjtaxer@gmail.com>
2021-06-23 14:19:44 -0400

    fixed duplicate uploads and image names that werent adding to the descrtiption dictionaries.  lines26-35 corrected comments added print messages to inform what the script is doing

--- run.py
+++ run.py
@@ -14,29 +14,33 @@ image_path = cwd + "/supplier-data/images/"
 # create a list of descriptions
 text_files = os.listdir(description_path)
 image_files = os.listdir(image_path)
-list_images = [image_name for image_name in image_files if '.jpeg' in image_name]
-# creats a list of files in path correctly
 
-# print(items)
-# print(images)
-#itterate through files in the directory and store as a dictionary
+# create a list of only the .jpeg files
+jpeg_images = [image_name for image_name in image_files if '.jpeg' in image_name]
+
+print('The following jpeg images will be used:')
+print(sorted(jpeg_images))
+print("")
+
+# create a list of dictionaries of fruit with their information
 list = []
 for file in text_files:
-        with open (description_path + file, 'r') as info:
-            data = (info.read().split('\n'))
-            file_dict = {'name':data[0], 'weight':int(data[1].split(" ")[0]), 'description':data[2]}
-            list.append(file_dict)
-
-for image in list_images:
-    if image.split('.')[0] in file.split('.')[0]:
-        file_dict['image_name']=image
+    with open (description_path + file, 'r') as info:
+        data = (info.read().split('\n'))
+        file_dict = {'name':data[0], 'weight':int(data[1].split(" ")[0]), 'description':data[2]}
         list.append(file_dict)
 
+    for image in jpeg_images:
+        if image.split('.')[0] in file.split('.')[0]:
+            file_dict['image_name']=image
+
+print('The following descriptions will be uploaded:')
+print(*list, sep = "\n" "\n")
+print("")
+
+#  Uploading the descriptions and images
 for item in list:
     resp = requests.post('http://34.134.195.119/fruits/', json=item)
     if resp.status_code != 201:
         raise Exception('POST error status={}'.format(resp.status_code))
     print('Created feedback ID: {}'.format(resp.json()["id"]))
-
-#print(*list, sep = "\n")
-print(list_images)
